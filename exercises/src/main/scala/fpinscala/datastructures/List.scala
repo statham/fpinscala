fcd166cbc41834ee3366dceae75bea7c860efe5a
Ky Statham <ky.statham@dominodatalab.com>
2018-03-05 18:07:21 -0800

    exercise 3.18

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -101,7 +101,8 @@ object List { // `List` companion object. Contains functions for creating and wo
     case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
   }
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = ???
+  def map[A,B](l: List[A])(f: A => B): List[B] =
+    foldRight(l, Nil:List[B])((h, t) => Cons(f(h), t))
 
   def reverse[A](l: List[A]): List[A] = {
     foldLeft(l, List[A]())((h, t) => Cons(t, h))
