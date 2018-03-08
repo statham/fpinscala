aaaaccc8ab6510469ec155870748324b92f4d50e
Ky Statham <ky.statham@dominodatalab.com>
2018-03-07 17:40:01 -0800

    exercise 3.22

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -132,4 +132,10 @@ object List { // `List` companion object. Contains functions for creating and wo
       if (f(h)) List(h)
       else Nil
     })
+
+  def addPairwise(a: List[Int], b: List[Int]): List[Int] = (a, b) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1 + h2, addPairwise(t1, t2))
+  }
 }
