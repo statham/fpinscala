f647e44d3891dda06d7fee1d363735beff3cd405
Ky Statham <ky.statham@dominodatalab.com>
2018-03-18 12:19:10 -0400

    exercise 4.3

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -54,7 +54,8 @@ object Option {
   def variance(xs: Seq[Double]): Option[Double] =
     mean(xs) flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a flatMap (realA => b map(realB => f(realA, realB)))
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = ???
 
